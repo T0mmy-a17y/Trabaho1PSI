593eeca56eb20ee0d7aa59b32314332b3a3d70b6
T0mmy-a17y <67442773+T0mmy-a17y@users.noreply.github.com>
2020-11-05 10:36:41 +0000

    Add files via upload

---
+++ ConsoleApp2/Entrypoint.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+using System.Threading;
+using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class EntryPoint
+    {
+        static Random rng = new Random();
+        
+
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Peter", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Mark", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rng.Next(0, 10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+
+                Thread.Sleep(500);
+
+
+            }
+        } 
+    }
+}
+
+
+
+ 
